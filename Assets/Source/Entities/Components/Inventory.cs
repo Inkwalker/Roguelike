20614a1917507466a040319f93259c1b356db528
Inkwalker <cockroachslidy@gmail.com>
2018-07-21 02:09:20 +0300

    Added inventory and item scripts

---
+++ Assets/Source/Entities/Components/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Roguelike.Entities
+{
+    public class Inventory : MonoBehaviour
+    {
+        [SerializeField] int capacity = 16;
+
+        public List<Item> Items { get; private set; }
+
+        private void Awake()
+        {
+            Items = new List<Item>(capacity);
+        }
+    }
+}
---
+++ Assets/Source/Entities/Components/Item.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Roguelike.Entities
+{
+    public class Item : MonoBehaviour
+    {
+    }
+}
