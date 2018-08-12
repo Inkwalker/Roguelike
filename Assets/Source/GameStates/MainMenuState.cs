64aaa4b64ec082f64f764ddbb3caeb4d84ae719a
Inkwalker <cockroachslidy@gmail.com>
2018-08-12 04:08:45 +0300

    Added main menu

---
+++ Assets/Source/GameStates/MainMenuState.cs
@@ -0,0 +1,62 @@
+﻿using Roguelike.UI;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Roguelike.GameStates
+{
+    public class MainMenuState : GameState
+    {
+        [SerializeField] string mainMenuScene = "MainMenu";
+
+        [SerializeField] GameState loadGameState;
+        [SerializeField] GameState newGameState;
+
+        private MainMenu menu;
+
+        public void LoadGame()
+        {
+            TransitionToState(loadGameState);
+        }
+
+        public void NewGame()
+        {
+            TransitionToState(newGameState);
+        }
+
+        private void OnEnable()
+        {
+            var menuScene = SceneManager.GetSceneByName(mainMenuScene);
+            if (menuScene.IsValid() == false)
+            {
+                SceneManager.LoadScene(mainMenuScene, LoadSceneMode.Additive);
+            }
+
+            StartCoroutine(FindRefs());
+        }
+
+        private void OnDisable()
+        {
+            if (menu != null)
+            {
+                menu.onLoadGame.RemoveListener(LoadGame);
+                menu.onNewGame.RemoveListener(NewGame);
+            }
+
+            SceneManager.UnloadSceneAsync(mainMenuScene);
+        }
+
+        IEnumerator FindRefs()
+        {
+            yield return null;
+
+            menu = FindObjectOfType<MainMenu>();
+
+            if (menu != null)
+            {
+                menu.onLoadGame.AddListener(LoadGame);
+                menu.onNewGame.AddListener(NewGame);
+            }
+        }
+    }
+}
---
+++ Assets/Source/UI/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+namespace Roguelike.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public UnityEvent onLoadGame;
+        public UnityEvent onNewGame;
+
+        public void OnContinueButton()
+        {
+            onLoadGame.Invoke();
+        }
+
+        public void OnNewGameButton()
+        {
+            onNewGame.Invoke();
+        }
+
+        public void OnExitButton()
+        {
+            Application.Quit();
+        }
+    }
+}
