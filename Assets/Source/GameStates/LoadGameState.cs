c30a6d5fd51b1708b13471ecaee71bad18296abf
Inkwalker <cockroachslidy@gmail.com>
2018-08-13 03:46:18 +0300

    Added LoadGameState

---
+++ Assets/Source/GameStates/LoadGameState.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Roguelike.Dungeon;
+using Roguelike.LoadSave;
+
+namespace Roguelike.GameStates
+{
+    public class LoadGameState : GameState
+    {
+        [SerializeField] string gameplayScene;
+        [SerializeField] GameState gameplayState;
+
+        private void OnEnable()
+        {
+            StartCoroutine(LoadScene());
+        }
+
+        IEnumerator LoadScene()
+        {
+            var scene = SceneManager.GetSceneByName(gameplayScene);
+
+            if (scene.IsValid())
+            {
+                var o = SceneManager.UnloadSceneAsync(scene);
+                yield return o;
+            }
+
+            SceneManager.LoadScene(gameplayScene, LoadSceneMode.Additive);
+
+            yield return null; //wait until all GameObjects are initialized
+
+            var gameMap = FindObjectOfType<GameMap>();
+
+            var storage = new GameSaveStorage();
+            var slot = storage.Read();
+
+            gameMap.CreateMap(slot.mapData);
+            gameMap.CreateEntites(slot.entities);
+
+            yield return null;
+
+            TransitionToState(gameplayState);
+        }
+    }
+}
